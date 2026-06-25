d42b43cb64bbafe8e8b850d2365e0a16b0698ea9
Zabir Arkam <zabirarkam27@users.noreply.github.com>
2026-06-25 22:18:00 +0600

    database connection added

---
+++ spotsync-api/config/db.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"os"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectDB() (*gorm.DB, error) {
+	dsn := os.Getenv("DATABASE_URL")
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, err
+	}
+
+	sqlDB.SetMaxOpenConns(25)
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetConnMaxLifetime(30 * time.Minute)
+
+	return db, nil
+}
